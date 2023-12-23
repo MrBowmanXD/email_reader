1aaae926d0b35d3712ded4de7ca7e7323650aa02
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2023-12-23 21:31:34 +0000

    Some functionalities to file manager

--- lib/file_opener.rb
+++ lib/file_opener.rb
@@ -1,4 +1,31 @@
-class FileOpener
-  def open
+class FileManager
+
+  def initialize(file, mode = 'r')
+    @file = File.open(file, mode)
+    @mode = mode
+  end
+
+  def read_content
+    @file_data = @file.read
+    @file_data
+  end
+
+  def print
+    puts read_content
+  end
+
+  def user_input
+    @user_input = gets.chomp
+  end
+
+  def write
+    if @mode == "w"
+      user_input
+      @file.write @user_input
+    end
+  end
+
+  def close
+    @file.close
   end
 end
--- spec/file_open_spec.rb
+++ spec/file_open_spec.rb
@@ -1,10 +1,33 @@
 require './lib/file_opener.rb'
 
-describe FileOpener do
-  describe "#open" do
+describe FileManager do
+  describe "#read_content" do
     it "returns the content of the file" do 
-      file_opener = FileOpener.new
-      expect(file_opener.open(something.txt)).to !eql(true)
+      file = FileManager.new("./documents/something.txt")
+      expect(file.read_content).not_to eql(nil)
+    end
+  end
+
+  describe "#print" do
+    it "to the screen the file content" do
+      file = FileManager.new("./documents/something.txt")
+      expect{ file.print }.to output.to_stdout
+    end
+  end
+
+  describe "#write" do
+    it "what the user input" do
+      file = FileManager.new("./documents/placeholder.txt", "w")
+      file.write
+      check_file = File.open("./documents/placeholder.txt", "r")
+      new_file = FileManager.new("./documents/placeholder.txt", "r")
+      expect(check_file.read).to eql(new_file.read_content)
+    end
+
+    it "failed cause of wrong mode" do
+      file = FileManager.new("./documents/placeholder.txt")
+      file.write
+      expect(file.read_content).not_to eql("Something")
     end
   end
 end
